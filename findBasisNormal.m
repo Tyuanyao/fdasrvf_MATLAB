fe09a76f5b1900b681c4686dcb4070c0b630806d
Yuanyao <tyuanyao@gmail.com>
2023-06-08 13:22:37 -0400

    rewrite findBasisNormal.m to work for 3D closed curve

--- findBasisNormal.m
+++ findBasisNormal.m
@@ -2,23 +2,28 @@ function basis = findBasisNormal(q)
 % Return basis vectors for normal space at q. basis is a cell array of size
 
 [n,T]=size(q);
+fs = zeros(n,T,n);
 
-f1=zeros(n,T);
-f2=zeros(n,T);
-for i=1:T
-    f1(:,i)=q(1,i)*q(:,i)/norm(q(:,i))+[norm(q(:,i));0];
-    f2(:,i)=q(2,i)*q(:,i)/norm(q(:,i))+[0;norm(q(:,i))];
-end
-h3=f1;
-h4=f2;
-integrandb3=zeros(1,T);
-integrandb4=zeros(1,T);
-for i=1:T
-    integrandb3(i)=q(:,i)'*h3(:,i);
-    integrandb4(i)=q(:,i)'*h4(:,i);
-end
-b3=h3-q*trapz(linspace(0,1,T),integrandb3);
-b4=h4-q*trapz(linspace(0,1,T),integrandb4);
+for i = 1:T
+    for j = 1:n
+        e = zeros(n,1);
+        e(j) = 1;
+        fs(:,i,j) = q(1,i)*q(:,i)/norm(q(:,i))+norm(q(:,i))*e;
 
-basis{1}=b3;
-basis{2}=b4;
+    end
+end
+
+integrandb = zeros(T,n);
+
+for i=1:T
+    for j = 1:n
+        integrandb(i,j) = q(:,i)'*fs(:,i,j);
+        integrandb(i,j) = q(:,i)'*fs(:,i,j);
+    end
+end
+
+basis = cell(3, 1);
+for j = 1:n
+    basis{j} = fs(:,:,j) - q*trapz(linspace(0,1,T),integrandb(:,j));
+end
+
