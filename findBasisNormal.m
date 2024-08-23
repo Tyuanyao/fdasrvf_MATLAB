51e22b1d82c203f1cf801b7d4bd97dd995b6a24f
tehya-tan <tyuanyao@gmail.com>
2024-08-23 14:59:16 -0400

    refine for n>3

--- findBasisNormal.m
+++ findBasisNormal.m
@@ -3,26 +3,27 @@ function basis = findBasisNormal(q)
 
 [n,T]=size(q);
 fs = zeros(n,T,n);
+integrandb = zeros(T,n);
 
 for i = 1:T
     for j = 1:n
         e = zeros(n,1);
         e(j) = 1;
         fs(:,i,j) = q(1,i)*q(:,i)/norm(q(:,i))+norm(q(:,i))*e;
-
-    end
-end
-
-integrandb = zeros(T,n);
-
-for i=1:T
-    for j = 1:n
-        integrandb(i,j) = q(:,i)'*fs(:,i,j);
         integrandb(i,j) = q(:,i)'*fs(:,i,j);
     end
 end
 
-basis = cell(3, 1);
+% integrandb = zeros(T,n);
+
+% for i=1:T
+%     for j = 1:n
+%         integrandb(i,j) = q(:,i)'*fs(:,i,j);
+%         integrandb(i,j) = q(:,i)'*fs(:,i,j);
+%     end
+% end
+
+basis = cell(n, 1);
 for j = 1:n
     basis{j} = fs(:,:,j) - q*trapz(linspace(0,1,T),integrandb(:,j));
 end
